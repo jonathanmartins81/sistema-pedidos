6896c936ffef31d999ec2095876920d671c782f1
jonathanmartins81 <jonathanmartins81@gmail.com>
2020-03-25 16:55:25 -0300

    \m04\04-reactzzaria\21\sistema-pedidos

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,35 +1,55 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import styled from 'styled-components'
 import firebase from 'firebase/app'
+import 'firebase/auth'
 import { Button, Grid } from '@material-ui/core'
 import { ReactComponent as MainLogo } from './logo-react-zzaria.svg'
 
 var config = {
-  apiKey: 'AIzaSyADubq8Pf-_nC6cM52PGZJLAJ_yT4UkWto',
-  authDomain: 'reactzzaria-12649.firebaseapp.com',
-  databaseURL: 'https://reactzzaria-12649.firebaseio.com',
-  projectId: 'reactzzaria-12649',
-  storageBucket: 'reactzzaria-12649.appspot.com',
-  messagingSenderId: '190402590347'
+  apiKey: 'AIzaSyCFXmX-8s_QOlDC0uAkW1NgjyGpEKsF2KU',
+  authDomain: 'react-zzaria-jw.firebaseapp.com',
+  databaseURL: 'https://react-zzaria-jw.firebaseio.com',
+  projectId: 'react-zzaria-jw',
+  storageBucket: 'react-zzaria-jw.appspot.com',
+  messagingSenderId: '1077009323103',
+  appId: '1:1077009323103:web:eca6d8ee6d5cb5290977ea',
+  measurementId: 'G-3HV3MG1L9Z'
 }
 
 firebase.initializeApp(config)
 
-const Login = () => (
-  <Container>
-    <Grid container justify='center' spacing={40}>
-      <Grid item>
-        <Logo />
-      </Grid>
+class Login extends PureComponent {
+  componentDidMount () {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log('usuário logado', user)
+      } else {
+        console.log('usuário não está logado', user)
+      }
+    })
+  }
 
-      <Grid item xs={12} container justify='center'>
-        <GitHubButton>
-          Entrar com GitHub
-        </GitHubButton>
-      </Grid>
-    </Grid>
-  </Container>
-)
+  render () {
+    return (
+      <Container>
+        <Grid container justify='center' spacing={40}>
+          <Grid item>
+            <Logo />
+          </Grid>
+
+          <Grid item xs={12} container justify='center'>
+            <GitHubButton onClick={() => {
+              const provider = new firebase.auth.GithubAuthProvider()
+              firebase.auth().signInWithRedirect(provider)
+            }}>
+              Entrar com GitHub
+            </GitHubButton>
+          </Grid>
+        </Grid>
+      </Container>
+    )
+  }
+}
 
 const Container = styled.div`
   padding: 20px;
